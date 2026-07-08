019a66cad3a635e33dc3a96f3f6dda8b93094527
kudu <kudu>
2026-07-08 21:57:18 +0200

    Fix static url

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from staticjinja import Site
 
 if __name__ == "__main__":
     site = Site.make_site(env_globals={
-        'STATIC_URL': '/site/',
+        'STATIC_URL': '/site',
     })
     site.render()
