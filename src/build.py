fbe47e17940432a6962f784d86dc5827ccc93f96
kudu <kudu>
2026-07-10 23:04:34 +0200

    Build

--- src/build.py
+++ src/build.py
@@ -1,5 +1,7 @@
 from staticjinja import Site
 
+from om_lib.template_code import decode_gw1_template
+
 if __name__ == "__main__":
     site = Site.make_site(
         searchpath="templates",
@@ -7,6 +9,7 @@ if __name__ == "__main__":
         staticpaths=["assets", "robots.txt", "favicon.ico"],
         env_globals={
             "BASE_URL": "/site/",
+            "decode_gw1_template": decode_gw1_template,
         },
     )
     site.render()
